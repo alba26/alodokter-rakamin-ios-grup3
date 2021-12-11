ed87d0987bbc23ba3f4350d66a7d5b0ba8312049
Aurelia Natasha <aurelianatashaaa@gmail.com>
2021-12-11 10:42:23 +0700

    add image carousel article page

---
+++ Alodokter_Rakamin_Bootcamp/Feature/ImageSliderCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageSliderCollectionViewCell.swift
+//  Alodokter_Rakamin_Bootcamp
+//
+//  Created by aurelia natasha on 11/12/21.
+//
+
+import UIKit
+
+class ImageSliderCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageSliderImg: UIImageView!
+    
+}
