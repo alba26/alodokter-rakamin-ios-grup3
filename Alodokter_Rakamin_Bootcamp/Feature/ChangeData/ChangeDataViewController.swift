fcddb46cf9b62e61372a037c1db6a66f341ba081
Finsen Antonius <finsenantonius42@gmail.com>
2021-12-12 22:00:38 +0700

    :lipstick: Update change data UI - P03

--- Alodokter_Rakamin_Bootcamp/Feature/ChangeData/ChangeDataViewController.swift
+++ Alodokter_Rakamin_Bootcamp/Feature/ChangeData/ChangeDataViewController.swift
@@ -9,16 +9,36 @@ import UIKit
 
 class ChangeDataViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
     
-    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var ktpTextField: UITextField!
+    @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var addressTextField: UITextField!
-    @IBOutlet weak var cancelMyData: UIBarButtonItem!
-    @IBOutlet weak var changeMyData: UIBarButtonItem!
+    @IBOutlet weak var genderTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBAction func closeChangeDataButton(_ sender: Any) {
+        closeModal()
+    }
     
+    let data = ["Pria", "Wanita"]
+    let genderData = [String](arrayLiteral: "Pria", "Wanita")
+    let picker = UIPickerView()
+    let datePicker = UIDatePicker()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createDatePicker()
+        genderTextField.inputView  = picker
+        genderTextField.setCustomUI(withPlaceholder: "Jenis kelamin")
+        nameTextField.setCustomUI(withPlaceholder: "Nama")
+        emailTextField.setCustomUI(withPlaceholder: "Email")
+        idTextField.setCustomUI(withPlaceholder: "KTP")
+        addressTextField.setCustomUI(withPlaceholder: "Alamat")
+        
+        picker.delegate = self
+        picker.dataSource = self
+    }
+    
+    // gender picker function
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -31,42 +51,42 @@ class ChangeDataViewController: UIViewController, UIPickerViewDataSource, UIPick
         return data[row]
     }
     
-    let data = ["Pria", "Wanita"]
-    
-    @IBAction func cancelButtonPressed(_ sender: Any) {
-        self.dismiss(animated: true){
-            
-        }
-    }
-    @IBOutlet weak var genderPicker: UIPickerView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.cancelMyData.tintColor = .white
-        self.changeMyData.tintColor = .white
-        genderPicker.dataSource = self
-        genderPicker.delegate = self
-        
-        if UserDefaults().checkSession() == Session.loggedIn.rawValue{
-            loadData()
-        }
-        
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        genderTextField.text = data[row]
+        genderTextField.resignFirstResponder()
     }
     
-    func loadData(){
-        if let data = UserDefaults.standard.data(forKey: "userdata"){
-            do{
-                let decoder = JSONDecoder()
-                let userdata = try decoder.decode(UserProfile.self, from: data)
-                nameTextField.text = userdata.firstname+" "+userdata.lastname
-                emailTextField.text = userdata.email
-                ktpTextField.text = userdata.identity
-                addressTextField.text = userdata.address
-            }catch{
-                print(error)
-            }
-        }
+    // birthday picker function
+    func createToolbar() -> UIToolbar {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
         
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolbar.setItems([doneButton], animated: true)
+        
+        return toolbar
+    }
+    
+    func createDatePicker() {
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.datePickerMode = .date
+        birthdayTextField.setCustomUI(withPlaceholder: "Tanggal lahir")
+        birthdayTextField.inputView =  datePicker
+        birthdayTextField.inputAccessoryView = createToolbar()
+    }
+    
+    @objc func donePressed() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        
+        self.birthdayTextField.text = dateFormatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
+    
+    
+    @objc func closeModal() {
+        self.dismiss(animated: true, completion: nil)
     }
     
 
