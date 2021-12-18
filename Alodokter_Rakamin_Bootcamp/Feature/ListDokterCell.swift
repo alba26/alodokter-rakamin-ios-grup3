0ab654105dae5dd23c716a35b5b40df0101f3e2a
Adhitya Laksamana Bayu Adrian <adhityalba@gmail.com>
2021-12-18 18:13:59 +0700

    add cell for list doctor

---
+++ Alodokter_Rakamin_Bootcamp/Feature/ListDokterCell.swift
@@ -0,0 +1,23 @@
+//
+//  ListDokterCell.swift
+//  Alodokter_Rakamin_Bootcamp
+//
+//  Created by Adhitya Laksamana Bayu Adrian on 18/12/21.
+//
+
+import UIKit
+
+class ListDokterCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
